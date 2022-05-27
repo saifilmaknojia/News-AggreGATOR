399a03ec8ac31e43cd087a417f9dfbff47d35304
saifilmaknojia <saifilmaknojia@gmail.com>
2022-05-26 20:35:30 -0700

    initial ejs templating

--- aggregator-node/index.js
+++ aggregator-node/index.js
@@ -3,24 +3,23 @@ const express = require("express");
 const app = express();
 const hostname = "127.0.0.1";
 const port = 3000;
+
 app.use(express.static(__dirname + "/assets/"));
 app.use(express.static(__dirname + "/js"));
+
+// set the view engine to ejs
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   // res.send("Hello World!");
   res.sendFile("view.html", { root: __dirname });
 });
 
+// News page
+app.get("/news", function (req, res) {
+  res.render("pages/index");
+});
+
 app.listen(port, function () {
   console.log("Started application on port %d", port);
 });
-
-/* const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-}); */
